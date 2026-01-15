31d1e64eb77e3580e60090faf0821cde02d54391
gopikrish1202 <68181485+gopikrish1202@users.noreply.github.com>
2026-01-15 18:59:55 +0530

    commit no.1
    changed localhost request to actual host port request

--- server.js
+++ server.js
@@ -116,6 +116,13 @@ app.delete("/todos/:id", async (req, res) => {
 });
 
 // Start server
-app.listen(3000, () =>
-  console.log("Server running on http://localhost:3000")
-);
+// app.listen(3000, () =>
+//   console.log("Server running on http://localhost:3000")
+// );
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+
