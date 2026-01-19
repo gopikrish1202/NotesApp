3463dad42c4e8384393c7e1f0a205858e3785a7e
gopikrish1202 <68181485+gopikrish1202@users.noreply.github.com>
2026-01-19 11:54:50 +0530

    added log for server port check

--- server.js
+++ server.js
@@ -120,9 +120,11 @@ app.delete("/todos/:id", async (req, res) => {
 //   console.log("Server running on http://localhost:3000")
 // );
 const PORT = process.env.PORT || 3000;
+ console.log(`Server running on port ${PORT}`);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
 
 
