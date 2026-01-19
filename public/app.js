9bdfa6bf4eaa6e6be920fb35ef5c56292af835ee
gopikrish1202 <68181485+gopikrish1202@users.noreply.github.com>
2026-01-19 22:06:00 +0530

    commit 15

--- public/app.js
+++ public/app.js
@@ -112,7 +112,7 @@ async function addTodo() {
   if (!res.ok) {
     const text = await res.text();
     console.error("Failed to add todo:", text);
-    alert("Error adding todo: " + text);
+    alert("Error adding todo: " + res);
     return;
   }
 
