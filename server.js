3d4acf6a42356e7bff59e40ad03328b1aaf7fc7e
gopikrish1202 <68181485+gopikrish1202@users.noreply.github.com>
2026-01-19 15:03:42 +0530

    user based data handling while pushing data to mongodb

--- server.js
+++ server.js
@@ -80,8 +80,10 @@ mongoose.connect(
 // Define schema
 const TodoSchema = new mongoose.Schema({
   name: String,
-  completed: Boolean
+  completed: Boolean,
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true }
 });
+
 
 // Create model
 const Todo = mongoose.model("Todo", TodoSchema);
